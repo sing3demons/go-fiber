1b5dc7242550378c2064cf6ea1302fe2cf37f467
kumneung <sing3demons@live.com>
2021-07-08 11:29:09 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module sing3demons/go-fiber
+
+go 1.16
+
+require github.com/gofiber/fiber/v2 v2.13.0
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+	app.Static("/", "./wwwroot")
+
+	app.Get("/api", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World")
+	})
+
+	app.Listen(":3000")
+}
